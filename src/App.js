88f1cb47716eca9fe02b568d03fe29b41e42c186
milosz.zajonc <milosz.zajonc@proficredit.pl>
2021-01-09 11:37:40 +0100

    JSX example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 
+const name = "Miłosz"
+const age = 16
+
 function App() {
   return (
     <div className="App">
-      <h2>Idle Clicker</h2>
+      <h1>Cześć {name}</h1>
+        <h2>Jesteś {age >= 18 ? 'Pełnoletni' : 'Niepełnoletni'}</h2>
     </div>
   );
 }
